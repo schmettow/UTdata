9bd6d7fa8cdc4f6fdf4809803c10ebc0b23215f4
Schmettow <m.schmettow@utwente.nl>
2020-02-18 15:24:51 +0100

    initial

---
+++ R/Data.R
@@ -0,0 +1,30 @@
+#' Infpump
+#'
+#' 34 X 107 Discovery matrix of testing a medical infusion pump
+
+"Infpump"
+
+#' Edu3D
+#'
+#' 20 X 119 Discovery matrix of testing a 3D educational software
+#'
+
+"Edu3D"
+
+#' Macerr
+#'
+#' 15 X 145 Discovery matrix
+
+"Macerr"
+
+#' Mantel
+#'
+#' 76 X 30 Discovery matrix
+
+"Mantel"
+
+#' Savings
+#'
+#' 34 X 44 Discovery matrix
+
+"Savings"
